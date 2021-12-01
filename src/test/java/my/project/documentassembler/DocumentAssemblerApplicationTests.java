9c7ef3f9fc56073274077cb41450476e9e27343e
Gabriel Felix de Souza Lopes <44716827+ga-felix@users.noreply.github.com>
2021-12-01 17:18:48 -0300

    Initial commit

---
+++ src/test/java/my/project/documentassembler/DocumentAssemblerApplicationTests.java
@@ -0,0 +1,13 @@
+package my.project.documentassembler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentAssemblerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
